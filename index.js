b7eab2f787f9e76013fd717112b569bd4e50fea4
Noel <fsigafoos@gmail.com>
2021-10-06 22:00:42 -0700

    Learned how to use JS and TS.

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+function scribbles() {
+    console.log("hello clover");
+}
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { hello } from "./src/hello"
+
+export function scribbles(): void{
+    hello("clover")
+    hello("hazel")
+    hello("tunnie")
+}
