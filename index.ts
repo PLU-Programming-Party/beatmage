3f9818536d827b438c5d1e65708d6b874983eaa7
StuartGavidia <stuarta0403@hotmail.com>
2021-11-10 22:11:10 -0800

    added loopdy to loopdtyLoop

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import { hello } from "./src/hello";
 import { rainConfetti } from "./src/confetti";
 import * as Tone from 'tone';
+import { Loop } from "tone";
 
 // TODO: Click to save beat
 
 let coolNoteOnIce = Tone.Frequency("C4");
 let hotBeatOnFire = 1;
-let loop: Tone.Loop
+let loopdy = scribbling();
+let looptdyLoop:Tone.Loop[] = [];
 
 window.scribbles = () => {
   hello("clover");
@@ -14,21 +16,28 @@ window.scribbles = () => {
   hello("tunnie");
   rainConfetti("canvas");
 
-  // create a new synth
-  const synth = new Tone.MembraneSynth().toMaster();
   // create a new tone loop
-  loop = new Tone.Loop(function (time) {
-    // Run once per eighth note, 8n, & log the time
-    console.log(time);
-    // trigger synth note
-    synth.triggerAttackRelease(coolNoteOnIce.toNote(), "4n");
-  }, hotBeatOnFire).start(0);
+  loopdy = scribbling();
   // Start the transport which is the main timeline
 
   Tone.Transport.start();
 
   // Tone.Transport.bpm.rampTo(1000, 1000)
 };
+function scribbling() {
+  const synth = new Tone.MembraneSynth().toMaster();
+  return new Tone.Loop(function (time) {
+    // Run once per eighth note, 8n, & log the time
+    console.log(time);
+    // trigger synth note
+    synth.triggerAttackRelease(coolNoteOnIce.toNote(), "4n");
+  }, hotBeatOnFire).start(0);
+}
+document.addEventListener("click", (event) => {
+  rainConfetti("canvas");
+  looptdyLoop.push(loopdy);
+  loopdy = scribbling();
+})
 
 document.addEventListener("mousemove", (event) => {
   let mousex = event.clientX; // Gets Mouse X
@@ -40,8 +49,8 @@ document.addEventListener("mousemove", (event) => {
   //bleatspeak
   hotBeatOnFire = 60 / normalizeToRange(mousey, 0, 512, 0, window.innerHeight);
   coolNoteOnIce = Tone.Frequency(normalizeToRange(mousex, 0, 127, 0, window.innerWidth), "midi");
-  if (loop) {
-    loop.interval = hotBeatOnFire;
+  if (loopdy) {
+    loopdy.interval = hotBeatOnFire;
   }
 });
 
