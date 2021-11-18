a81899baa93091d172fd346838bd40dcaaa03809
AJ Moore <mooreajj@gmail.com>
2021-11-17 22:19:21 -0800

    feat: stopped wasting marimbas

--- index.ts
+++ index.ts
@@ -3,11 +3,12 @@ import { rainConfetti } from "./src/confetti";
 import * as Tone from "tone";
 
 // TODO: Click to save beat
-
-let coolNoteOnIce = Tone.Frequency("C4");
-let hotBeatOnFire = 1;
-let loopdy = scribbling();
+Tone.Transport.start();
+let coolestNoteOnIce = Tone.Frequency("C2");
+let hottestBeatOnFire = 1;
+let loopdy = scribbling(undefined, hottestBeatOnFire);
 let looptdyLoop: Tone.Loop[] = [];
+const synth = new Tone.MembraneSynth().toMaster();
 
 window["scribbles"] = () => {
   hello("clover");
@@ -15,47 +16,50 @@ window["scribbles"] = () => {
   hello("tunnie");
   rainConfetti("canvas");
 
-  // create a new tone loop
-  loopdy = scribbling();
-  // Start the transport which is the main timeline
-
-  Tone.Transport.start();
-
   // Tone.Transport.bpm.rampTo(1000, 1000)
 };
 
-function scribbling() {
-  const synth = new Tone.MembraneSynth().toMaster();
+function scribbling(coolerNoteOnIce: Tone.FrequencyClass<number> | undefined, hotterBeatOnFire: number) {
+
   return new Tone.Loop(function (time) {
     // Run once per eighth note, 8n, & log the time
     console.log(time);
     // trigger synth note
-    synth.triggerAttackRelease(coolNoteOnIce.toNote(), "4n");
-  }, hotBeatOnFire).start(0);
+    synth.triggerAttackRelease((coolerNoteOnIce ?? coolestNoteOnIce).toNote(), "4n");
+  }, hotterBeatOnFire).start(0);
 }
+
+ document.addEventListener("mousemove", (event) => {
+   let mousex = event.clientX;
+   let mousey = event.clientY;
+   hottestBeatOnFire = 60 / normalizeToRange(mousey, 0, 512, 0, window.innerHeight);
+   coolestNoteOnIce = Tone.Frequency(
+    normalizeToRange(mousex, 0, 127, 0, window.innerWidth),
+    "midi"
+   );
+   if (loopdy) {
+    loopdy.interval = hottestBeatOnFire;
+   }
+   
+ });
 
 document.addEventListener("click", (event) => {
   rainConfetti("canvas");
-  looptdyLoop.push(loopdy);
-  loopdy = scribbling();
-});
-
-document.addEventListener("mousemove", (event) => {
   let mousex = event.clientX; // Gets Mouse X
   let mousey = event.clientY; // Gets Mouse Y
   console.log([mousex, mousey]); // Prints data
   var now = Tone.Transport.now();
   // TODO:　tame them beatz yo
-  //Tone.Transport.bpm.setValueAtTime(normalizeToRange(mousey, 0, 1/1000, 0, window.innerHeight), now);
-  //bleatspeak
-  hotBeatOnFire = 60 / normalizeToRange(mousey, 0, 512, 0, window.innerHeight);
-  coolNoteOnIce = Tone.Frequency(
+  // Tone.Transport.bpm.setValueAtTime(normalizeToRange(mousey, 0, 1/1000, 0, window.innerHeight), now);
+  // bleatspeak
+
+  const hotBeatOnFire = 60 / normalizeToRange(mousey, 0, 512, 0, window.innerHeight);
+  const coolNoteOnIce = Tone.Frequency(
     normalizeToRange(mousex, 0, 127, 0, window.innerWidth),
     "midi"
   );
-  if (loopdy) {
-    loopdy.interval = hotBeatOnFire;
-  }
+  scribbling(coolNoteOnIce, hotBeatOnFire);
+
 });
 
 function normalizeToRange(
