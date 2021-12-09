4de6e7d0e2dde6e423e2c3f4d2f30d353eb6314a
StuartGavidia <stuarta0403@hotmail.com>
2021-12-08 22:08:22 -0800

    Added rad drummer installed jquery added onsubmit to button bringTheNoys Fixed the bubbling bug

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
 import { hello } from "./src/hello";
 import { rainConfetti } from "./src/confetti";
 import * as Tone from "tone";
+import $ from "jquery";
 
-window["scribbles"] = async () => {
+$("#thescribler").on("click", event => {
+  event.stopPropagation()
+  bringTheNoys()
+})
+
+
+async function bringTheNoys() {
+  $("#thescribler").hide();
+
   hello("clover");
   hello("hazel");
   hello("tunnie");
@@ -20,41 +29,39 @@ window["scribbles"] = async () => {
 
     return new Tone.Loop(function (time) {
       // Run once per eighth note, 8n, & log the time
-      console.log(time);
       // trigger synth note
       synth.triggerAttackRelease((coolerNoteOnIce ?? coolestNoteOnIce).toNote(), "4n");
     }, hotterBeatOnFire).start(0);
   }
-  
-   document.addEventListener("mousemove", (event) => {
-     let mousex = event.clientX;
-     let mousey = event.clientY;
-     hottestBeatOnFire = 60 / normalizeToRange(mousey, 0, 512, 0, window.innerHeight);
-     coolestNoteOnIce = Tone.Frequency(
+
+  document.addEventListener("mousemove", (event) => {
+    let mousex = event.clientX;
+    let mousey = event.clientY;
+    hottestBeatOnFire = 60 / normalizeToRange(mousey, 0, 512, 0, window.innerHeight);
+    coolestNoteOnIce = Tone.Frequency(
       normalizeToRange(mousex, 0, 127, 0, window.innerWidth),
       "midi"
-     );
-     if (loopdy) {
+    );
+    if (loopdy) {
       loopdy.interval = hottestBeatOnFire;
-     }
-   });
-  
+    }
+  });
   document.addEventListener("click", (event) => {
     rainConfetti("canvas");
     let mousex = event.clientX; // Gets Mouse X
     let mousey = event.clientY; // Gets Mouse Y
     console.log([mousex, mousey]); // Prints data
     var now = Tone.Transport.now();
-  
+
+
     const hotBeatOnFire = 60 / normalizeToRange(mousey, 0, 256, 0, window.innerHeight);
     const coolNoteOnIce = Tone.Frequency(
       normalizeToRange(mousex, 0, 127, 0, window.innerWidth),
       "midi"
     );
     scribbling(coolNoteOnIce, hotBeatOnFire);
-  
   });
-  
+
   function normalizeToRange(
     value: number,
     targetMin: number,
